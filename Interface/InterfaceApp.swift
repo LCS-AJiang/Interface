87144111be090b2615b6de7fc9424eb959c6027d
Audrey Jiang <ajiang@lcs.on.ca>
2022-11-18 08:48:32 -0500

    Initial Commit

---
+++ Interface/InterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterfaceApp.swift
+//  Interface
+//
+//  Created by Audrey Jiang on 2022-11-18.
+//
+
+import SwiftUI
+
+@main
+struct InterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
