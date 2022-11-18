3b5db095476ea61c4dffe27c27371b901c1b3ee0
Audrey Jiang <ajiang@lcs.on.ca>
2022-11-18 10:04:28 -0500

    no shown

--- Interface/ContentView.swift
+++ Interface/ContentView.swift
@@ -9,18 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            
+            // First layer (background)
+            Color.black
+                .ignoresSafeArea()
+            
+            // Second layer (rest of interface)
+            VStack {
+                Spacer()
+                Spacer()
+                Text("Welcome to")
+                    .font(Font.system(size: 40, weight: .bold))
+                    .foregroundColor(.white)
+                
+                Text("Interval Pro")
+                    .font(Font.system(size: 45, weight: .bold))
+                    .foregroundColor(.blue)
+                Spacer()
+                
+                VStack {
+                    HStack {
+                        ContentView()
+                            .tabItem {
+                                Image(systemName: "Timer")
+                                Text("Custom Interval Timers")
+                                    .font(Font.system(size: 20))
+                                    .foregroundColor(.white)
+                                
+                                
+                                
+                            }
+                    }
+                    .padding()
+                }
+            }
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+            
+        }
     }
 }
